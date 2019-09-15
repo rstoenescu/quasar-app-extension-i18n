e2f6784ddd19331c0ee0f4f252ab9ae0e7f8fc9d
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2019-09-15 13:30:56 +0300

    Update i18n.ts

--- src/templates/src/boot/i18n.ts
+++ src/templates/src/boot/i18n.ts
@@ -2,7 +2,8 @@ import VueI18n from 'vue-i18n';
 
 import messages from 'src/i18n';
 
+Vue.use(VueI18n);
+
 export default async ({ app, Vue }) => {
-  Vue.use(VueI18n);
   app.i18n = new VueI18n({ messages, fallbackLocale: 'en-us', locale: 'en-us', silentFallbackWarn: true });
 };
